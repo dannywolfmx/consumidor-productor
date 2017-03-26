352c4a8d36ca0fd5457489a61d00fdd0edcc0cff
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2017-03-26 14:58:57 -0500

    Update CustomOutputStream.scala

--- src/main/scala/CustomOutputStream.scala
+++ src/main/scala/CustomOutputStream.scala
@@ -7,22 +7,6 @@ import java.io.PrintStream
 
 
 
-/*public class CustomOutputStream extends OutputStream {
-    private JTextArea textArea;
-     
-    public CustomOutputStream(JTextArea textArea) {
-        this.textArea = textArea;
-    }
-     
-    @Override
-    public void write(int b) throws IOException {
-        // redirects data to the text area
-        textArea.append(String.valueOf((char)b));
-        // scrolls the text area to the end of data
-        textArea.setCaretPosition(textArea.getDocument().getLength());
-    }
-}*/
-
 class CustomOutputStream(textArea:TextArea,old:PrintStream) extends OutputStream{
   
   
